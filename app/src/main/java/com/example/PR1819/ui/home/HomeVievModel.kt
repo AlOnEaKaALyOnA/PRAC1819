d7f8a5f50b4cf68854d00dbadf70dac4b09d7f82
Alone <sergij.1230@gmail.com>
2022-11-11 18:53:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/PR1819/ui/home/HomeVievModel.kt
@@ -0,0 +1,29 @@
+package com.example.PR1819.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeVievModel: ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "        В данной статье я расскажу об основных компонентах типичного домашнего (стационарного) компьютера для того, чтобы вы имели представление как он устроен, как выглядит тот или иной компонент, какие имеет характеристики и за что отвечает. Вся эта информация может пригодиться простым начинающим пользователям при выборе и покупке компьютера… Под «Основными» я имел ввиду те компоненты (комплектующие), которые вынимаются и подлежат простой замене. Проще говоря, я не буду заходить слишком далеко и рассказывать в детальных подробностях, как работает компьютер, объясняя каждый элемент на платах и внутреннее устройство каждого компонента. Данный блог читает очень много новичков, и я считаю, что сразу говорить обо всех сложных процессах и терминах – это не есть хорошо и просто вызовет кашу в голове."
+    }
+    val text: LiveData<String> = _text
+
+    /*private  val _image = MutableLiveData<Image>().apply {
+        value = R.id.
+
+    }*/
+
+    /*private var _wallpaper = MutableLiveData<Drawable>()
+    val wallpaper: LiveData<Drawable>
+        get() = _wallpaper
+
+    fun updateWallPaper() {
+        val file = appCtx.getWallpaperFile()
+        if(file.exists()) {
+            _wallpaper.value = BitmapDrawable(R.drawable.campic, BitmapFactory.decodeFile(file.absolutePath))
+        }
+    }*/
+
+}
